890335fcdfb193c2b92d72e27399c79b2dec4c55
Martin Kinaro <njengamartin72@gmail.com>
2021-07-29 09:00:59 +0300

    First Commit

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "This project was a property management system for a client. Will upload link in future.",
+    image: "./project-2.gif",
+    link: "#",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description: "An experimental react firebase project up and coming",
+    image: "./project-3.gif",
+    link: "#",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "#",
+  },
+];
+
+export const testimonials = [
+  {
+    quote: "Did a very good job. Love the passion and accountability.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Chemutai",
+    company: "Student",
+  },
+  {
+    quote: "I like how Martin is dedicated to his work.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Watson",
+    company: "Property Merchants",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "Python",
+  "Django",
+  "Flask",
+  "Material UI",
+];
